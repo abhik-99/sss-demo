95a44d1e8d6758a2aa328606aff3f1087be363bd
Abhik Banerjee <abhik.banerjee.1999@gmail.com>
2022-10-06 17:00:23 +0530

    First Commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FormEvent } from 'react';
 import './App.css';
+const sss = require('shamirs-secret-sharing')
+
 
 function App() {
+  const [walletSeed, setWalletSeed] = React.useState<string>('')
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log("Submitted", walletSeed)
+    const secret = Buffer.from(walletSeed)
+    const shares = sss.split(secret, { shares: 10, threshold: 4 })
+    const stringShares: string[] = [];
+    shares.forEach( (share:Uint8Array) => {
+      stringShares.push(share.join("//"))
+    })
+    
+    console.log("Shares", shares);
+    console.log("String shares", stringShares);
+
+    const properShare: Uint8Array[] = [];
+    stringShares.forEach(share => {
+      properShare.push(Uint8Array.from(share.split("//").map( x => parseInt(x))))
+    })
+
+    console.log("From String Shares", properShare);
+
+    console.log(shares[0].toString())
+
+    const recovered = sss.combine(shares.slice(3, 7))
+    console.log("Recovered", recovered.toString()) // 'secret key'
+
+    const recovered2 = sss.combine(properShare.slice(3,7));
+    console.log(recovered2.toString())
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="App-body">
+        <h2>Shamir Secret Sharing Demo</h2>
+        <form onSubmit={handleSubmit}>
+        <input className="form__input" type="text" placeholder='Enter Secret' onChange={(e: React.ChangeEvent<HTMLInputElement>) => setWalletSeed(e.target.value)}/>
+        <button type="submit" className='button-32'>
+          Create Shards
+        </button>
+        </form>
+      </main>
     </div>
   );
 }
